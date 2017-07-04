5c8b323b8cf8b94fb85ef8a9a3dc13956a7c59c0
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-04 21:54:22 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+   "b2xVn2": "http://www.lighthouselabs.ca",
+   "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  res.render("urls_index", { urls: urlDatabase 
+  });
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
