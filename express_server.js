8068cc0ca006927369fcbcb492f67eb9308d65c0
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-05 05:03:05 +0000

    updated code for TinyApp, finished day 1 for tin app

--- express_server.js
+++ express_server.js
@@ -2,12 +2,25 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
    "b2xVn2": "http://www.lighthouselabs.ca",
    "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(webaddress) {
+ var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,9 +35,28 @@ app.get("/urls", (req, res) => {
   });
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const newString = generateRandomString()
+  urlDatabase[newString] = req.body.longURL;
+  console.log(urlDatabase);
+  console.log(req.body);  // debug statement to see POST parameters
+  console.log(newString);
+  res.send("Ok, Hi there, here is your new URL: http://localhost:8080/urls/" + newString);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", templateVars);
+  res.render("urls_show", { shortURL: req.params.id, longURL: urlDatabase[req.params.id]
+  });
 });
 
 app.get("/urls.json", (req, res) => {
@@ -32,5 +64,5 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Bone Saw is ready ${PORT}!`);
 });
