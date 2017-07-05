8fac6b4a585cecd140f35bfc7be88e5ab253a28c
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-05 20:41:11 +0000

    reviewed and debugged

--- express_server.js
+++ express_server.js
@@ -3,16 +3,16 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-   "b2xVn2": "http://www.lighthouselabs.ca",
-   "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString(webaddress) {
- var text = "";
+function generateRandomString() {
+  var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 6; i++)
@@ -23,16 +23,11 @@ function generateRandomString(webaddress) {
 
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase 
-  });
+  res.render("urls_index", { urls: urlDatabase });
 });
 
 app.get("/urls/new", (req, res) => {
@@ -42,25 +37,19 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const newString = generateRandomString()
   urlDatabase[newString] = req.body.longURL;
-  console.log(urlDatabase);
-  console.log(req.body);  // debug statement to see POST parameters
-  console.log(newString);
-  res.send("Ok, Hi there, here is your new URL: http://localhost:8080/urls/" + newString);         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls/" + newString);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]
-  console.log(longURL);
+  //todo:if long URL is undefined, show 404 instead of redirecting
   res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { shortURL: req.params.id, longURL: urlDatabase[req.params.id]
+  res.render("urls_show", {
+    shortURL: req.params.id, longURL: urlDatabase[req.params.id]
   });
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
