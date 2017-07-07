3f94e2a78d212d1b7aafc9e613ef3d69764c3b4c
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-07 23:30:44 +0000

    updated code, cleaned syntax, and object access

--- express_server.js
+++ express_server.js
@@ -9,7 +9,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-
 app.use((req, res, next) => {
   res.locals = {
     user_id: req.cookies["user_id"]
@@ -18,16 +17,22 @@ app.use((req, res, next) => {
 });
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "shortURL": {
+    "creator": "user_id",
+    "longURL": "http://www.lighthouselabs.ca",
+  }
+}
+//urlsDatabase[id - form the cooke].url should 
+// must be dynamic user_id: {
 
 const users = {
   userRandomID1: {
+    id: "userRandomID1",
     email: "juanvictor.cortez@gmail.com",
     password: "asdf",
   },
   userRandomID2: {
+    id: "userRandomID2",
     email: "juanvictorcortez@yahoo.ca",
     password: "qwer"
   }
@@ -45,7 +50,7 @@ function generateRandomString() {
 
 function generateRandomIdNumber() {
   var text = "";
-  var possible ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 6; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -71,22 +76,55 @@ function findUserPassword(password) {
   return undefined
 }
 
+function findUserID(id) {
+  let filteredUrls = {};
+  for (let shortURL in urlDatabase) {
+    const url = urlDatabase[shortURL];
+    if (url.user_id == id) {
+      filteredUrls[shortURL] = url;
+    }
+  }
+  return filteredUrls;
+}
+
 app.get("/", (req, res) => {
-  res.redirect("/urls");
+  var foundUserID = findUserID();
+  if (foundUserID !== undefined) {
+    res.redirect("login")
+  } else {
+    res.render("urls_index", { urls: urlDatabase });
+  }
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase });
+  var foundUserID = findUserID();
+  if (foundUserID !== undefined) {
+    res.redirect("/login")
+  } else {
+    res.render("urls_index", { urls: urlDatabase });
+  }
 });
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+// if (urlDatabase[req.params.id].creator !== req.cookies["user_id"]) {
+//   res.status(404).send('Not logged in');
+// } else if (urlDatabase[req.params.id].creator === req.cookies["user_id"]) {
+//   res.redirect("/urls_index");
+// } else {
+//   res.redirect("/urls_login");
+// }
 
 app.post("/urls", (req, res) => {
   const newString = generateRandomString()
-  urlDatabase[newString] = req.body.longURL;
+  urlDatabase[newString] = {
+    "longURL": req.body.longURL,
+    "creator": req.cookies["user_id"]
+  };
   res.redirect("/urls/" + newString);
+});
+//Cannot read property 'grnAJn' of undefined address this bug 
+app.get("/urls/new", (req, res) => {
+  if (req.cookies.user_id === undefined) {
+    res.redirect("/register")
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -100,7 +138,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   res.render("urls_show", {
-    shortURL: req.params.id, longURL: urlDatabase[req.params.id]
+    shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]
   });
 });
 
@@ -117,26 +155,26 @@ app.post("/register", (req, res) => {
     res.status(400).send("Invalid email or email exists. Please login instead")
   } else {
     const user_id = generateRandomIdNumber();
-    users[user_id] = { email, password };
+    users[user_id] = { id: user_id, email, password };
     const newRandomId = users[user_id];
     res.cookie("user_id", newRandomId);
     res.redirect("/urls")
+    console.log("random ID", newRandomId)
   }
 });
 
 app.get("/login", (req, res) => {
-  res.render("urls_login", {
-    user_id: req.params.login
-  })
+  res.render("urls_login")
 })
 
 app.post("/login", (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body
   const user = findUser(email)
   // const userPassword = findUserPassword(password)
-  if (user && user.password === password) {
+  if (user && user.password === password) {;
     res.cookie("user_id", user.id)
-    res.redirect("/urls")
+    res.locals.user_id = user.id
+    res.render("urls_index", { urls: urlDatabase })
   } else {
     res.status(403).send("Please login")
   }
@@ -144,17 +182,18 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id")
-  res.redirect("/urls")
+  res.redirect("login")
 })
 
 app.post('/urls/:id/delete', (req, res) => {
   const shortURL = req.params.id;
-  console.log(urlDatabase[shortURL]);
   if (urlDatabase[shortURL] === undefined) {
     res.status(404).send('No existing URL');
+  } else if (urlDatabase[req.params.id].creator === req.cookies["user_id"]) {
+    delete urlDatabase[req.params.id];
+    res.redirect("urls");
   } else {
-    delete urlDatabase[shortURL];
-    res.redirect('/urls');
+    res.send("Invalid user")
   }
 });
 
