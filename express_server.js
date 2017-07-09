7f4af64e42ec664ac99b82f083319ae326f59141
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-09 18:31:10 +0000

    added bcrypt to hash passwords

--- express_server.js
+++ express_server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+// const cookieParser = require('cookie-parser')
+// app.use(cookieParser())
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  secret: 'pinkunicorns',
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
 
 app.use((req, res, next) => {
   res.locals = {
@@ -15,6 +22,11 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+const bcrypt = require('bcrypt');
+const salt = 10;
+// need to add hash to encrypt password
+// console.log("here are the pw", password, hashed_password, compareSync(password, hashed_password))
 
 const urlDatabase = {
   "shortURL": {
@@ -88,8 +100,7 @@ function findUserID(id) {
 }
 
 app.get("/", (req, res) => {
-  var foundUserID = findUserID();
-  if (foundUserID !== undefined) {
+  if (req.cookies["user_id"] === undefined) {
     res.redirect("login")
   } else {
     res.render("urls_index", { urls: urlDatabase });
@@ -97,20 +108,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  var foundUserID = findUserID();
-  if (foundUserID !== undefined) {
+  if (req.cookies["user_id"] === undefined) {
     res.redirect("/login")
   } else {
     res.render("urls_index", { urls: urlDatabase });
   }
 });
-// if (urlDatabase[req.params.id].creator !== req.cookies["user_id"]) {
-//   res.status(404).send('Not logged in');
-// } else if (urlDatabase[req.params.id].creator === req.cookies["user_id"]) {
-//   res.redirect("/urls_index");
-// } else {
-//   res.redirect("/urls_login");
-// }
 
 app.post("/urls", (req, res) => {
   const newString = generateRandomString()
@@ -120,19 +123,21 @@ app.post("/urls", (req, res) => {
   };
   res.redirect("/urls/" + newString);
 });
-//Cannot read property 'grnAJn' of undefined address this bug 
+
 app.get("/urls/new", (req, res) => {
-  if (req.cookies.user_id === undefined) {
-    res.redirect("/register")
+  if (req.cookies["user_id"] === undefined) {
+    res.redirect("/login")
+  } else {
+    res.render("urls_new")
   }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL]
-  if (longURL === undefined) {
+  let longURLObject = urlDatabase[req.params.shortURL]
+  if (longURLObject.longURL === undefined) {
     res.status(404).send('No existing URL');
   } else {
-    res.redirect(longURL);
+    res.redirect(longURLObject.longURL);
   }
 });
 
@@ -151,15 +156,19 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
   const user = findUser(email)
-  if (user) {
+  if (user !== undefined) {
     res.status(400).send("Invalid email or email exists. Please login instead")
   } else {
-    const user_id = generateRandomIdNumber();
-    users[user_id] = { id: user_id, email, password };
-    const newRandomId = users[user_id];
-    res.cookie("user_id", newRandomId);
-    res.redirect("/urls")
-    console.log("random ID", newRandomId)
+    bcrypt.hash(password, salt, function (err, hash) {
+      if (err) {
+        res.send("There was an error creating your account.")
+        return
+      }
+      const user_id = generateRandomIdNumber();
+      users[user_id] = { id: user_id, email, password: hash };
+      res.cookie("user_id", password);
+      res.redirect("/login")
+    })
   }
 });
 
@@ -170,33 +179,36 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   const { email, password } = req.body
   const user = findUser(email)
-  // const userPassword = findUserPassword(password)
-  if (user && user.password === password) {;
-    res.cookie("user_id", user.id)
-    res.locals.user_id = user.id
-    res.render("urls_index", { urls: urlDatabase })
+  bcrypt.compare(password, user.password, function (err, matched) {
+    if (matched) {
+      console.log("compare", matched)
+      res.cookie("user_id", user.id);
+      res.locals.user_id = user.id;
+      res.render("urls_index", { urls: urlDatabase });  
   } else {
-    res.status(403).send("Please login")
-  }
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id")
-  res.redirect("login")
+      res.status(403).send("Email not found. Please register. You won't regret it!");
+    }
+  })
 })
 
-app.post('/urls/:id/delete', (req, res) => {
-  const shortURL = req.params.id;
-  if (urlDatabase[shortURL] === undefined) {
-    res.status(404).send('No existing URL');
-  } else if (urlDatabase[req.params.id].creator === req.cookies["user_id"]) {
-    delete urlDatabase[req.params.id];
-    res.redirect("urls");
-  } else {
-    res.send("Invalid user")
-  }
-});
+  app.post("/logout", (req, res) => {
+    res.clearCookie("user_id")
+    res.redirect("login")
+  })
 
-app.listen(PORT, () => {
-  console.log(`What time is it? It's ${PORT} time!`);
-});
+  app.post('/urls/:id/delete', (req, res) => {
+    const shortURL = req.params.id;
+    console.log(shortURL)
+    if (urlDatabase[shortURL] === undefined) {
+      res.status(404).send('No existing URL');
+    } else if (urlDatabase[req.params.id].creator === req.cookies["user_id"]) {
+      delete urlDatabase[req.params.id];
+      res.redirect("/");
+    } else {
+      res.send("Invalid user")
+    }
+  });
+
+  app.listen(PORT, () => {
+    console.log(`What time is it? It's ${PORT} time!`);
+  });
